fd51173b569154df0fa1a060ac35a849d8b8c452
ziomber <ziomber@yahoo.com>
2025-08-14 13:18:34 +0200

    Update dnf_updates_security_count.sh

--- scripts/dnf_updates_security_count.sh
+++ scripts/dnf_updates_security_count.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-COUNT=$(dnf -q updateinfo list security 2>/dev/null | grep -v '^Last metadata' | wc -l)
+COUNT=$(dnf -q check-update --security 2>/dev/null | grep -v '^Last metadata' | wc -l)
 
 if [[ -z "$COUNT" ]]; then
     COUNT=0
